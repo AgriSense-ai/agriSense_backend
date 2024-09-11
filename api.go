7a8596c143f15cb2b6dd526fd8823122927fce6c
ConradKash <kakurucon1234@gmail.com>
2024-09-11 21:36:07 +0300

    Refactor API server to handle different HTTP methods for account endpoints

--- api.go
+++ api.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
-
-	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
 }
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
 type APIError struct {
-	Error string 
+	Error string
 }
 
-func makeHTTPHandler(f apiFunc) http.HandlerFunc{
+func makeHTTPHandler(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
 			WriteJSON(w, http.StatusInternalServerError, APIError{Error: err.Error()})
@@ -42,29 +43,46 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandler(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandler(s.handleAccount))
+
+	log.Printf("API server listening on %s", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
 }
 
-func (s *APIServer) handleAccount(w http.ResponseWriter, r http.Request) error {
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("not implemented %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+
+	fmt.Println(id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
+
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 
 	return nil
 }
 
-func (s *APIServer) handleGetAccount(w http.ResponseWriter, r http.Request) error {
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 
 	return nil
 }
 
-func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r http.Request) error {
-
-	return nil
-}
-
-func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r http.Request) error {
-
-	return nil
-}
-
-func (s *APIServer) handleTransfer(w http.ResponseWriter, r http.Request) error {
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module agrisense_backend
 
 go 1.23.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
